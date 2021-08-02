3cd3b15dd6c493f022198986b70eac2b6d4406e6
Vanille-N <neven.villani@gmail.com>
2021-08-02 10:34:03 +0200

    subdivide regeneration of structure

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+CFG = Makefile htmldef.sh
+
 all: sync
 
 sync: sharestruct
@@ -11,13 +13,29 @@ sharestruct: $(SHARE:%=%/index.html)
 FILES = $(shell find www/share -type f -not -name '*.html')
 sharefiles: $(FILES:%=%.html)
 
-%/index.html: % $(wildcard %/*) Makefile mkstruct htmldef.sh
+#subdirs.mk: $(FILES) Makefile
+#	: > subdirs.mk
+#	for d in $$(find www/share -type d); do\
+#		echo "$$d/index.html: $$(find $$d/ | tr '\n' ' ')" >> subdirs.mk; \
+#	done
+
+#include subdirs.mk
+%/index.html: % %/index-ls.html %/index-tree.html mkstruct $(CFG)
 	./mkstruct $<
 
-%.html: % Makefile mkbat htmldef.sh
+%/index-ls.html: % $(FILES) mkls $(CFG)
+	./mkls $<
+
+%/index-tree.html: % $(SHARE) mktree $(CFG)
+	./mktree $<
+
+%.html: % mkbat $(CFG)
 	./mkbat $<
 
 clean:
-	find www/share -name '*.html' -exec rm {} +
+	find www/share \
+		-name '*.html' \
+		-and -not -name 'extra.html' \
+		-exec rm {} +
 
-.PHONY: sync share sharestruct sharefiles
+.PHONY: sync share sharestruct sharefiles clean
--- htmldef.sh
+++ htmldef.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Configuration & wrappers for mkbat + mkstruct
+
+# Header: title, stylesheet, color codes, open ANSI block
 HEADER="\
 <head>
     <meta charset=\"utf-8\">
@@ -13,20 +16,27 @@ HEADER="\
     <pre>
 "
 
+# Wrapper around ansi2html for uniformized conversion
+# --body-only: header is in www/colorcodes.css
+ansi_cvt() {
+    ansi2html --bg=dark --body-only
+}
 
-ansi_cvt() { ansi2html --bg=dark --body-only; }
+# Safe manual ANSI formatting
 span_class() { echo -n "<span class=\"$1\">$2</span>"; }
 bold() { span_class 'bold' "$1"; }
 yellow() { span_class 'f3' "$1"; }
 green() { span_class 'f2' "$1"; }
 blue() { span_class 'f4' "$1"; }
 
+# Some  more html wrappers
 style() { echo -n "<span style=\"$1\">"; }
-
 href() { echo -n "<a href=\"$1\">$2</a>"; }
 
+# Regex to select an item _and_ all formatting around
 re_span() { echo -n "(<span[^>]*>)*$1(</span>)*"; }
 
+# links:  .  ..  /  /share
 NAVIGATION="\
 $(style "line-height:1.1")\
      $(href "./index.html" "$(blue "$(bold ".")")")\
@@ -35,6 +45,7 @@ $(style "line-height:1.1")\
           $(href "$ROOT/share/index.html" "$(blue "$(bold "/share")")")\
 </span>"
 
+# Footer: ref to github, close ANSI block
 FOOTER="\
 $(style "font-size:70%")
 Powered by $(href "https://github.com/Vanille-N/website" 'Vanille-N/website')
